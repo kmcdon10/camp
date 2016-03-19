e57b56d2ca0b078590f37a500443f3958ab614de
Kate <katemcdonald39@gmail.com>
2016-03-19 00:35:37 -0400

    finished setting up paypal test and added some more content to html pages

--- controllers/default.py
+++ controllers/default.py
@@ -25,6 +25,18 @@ def user():
     """
     return dict(form=auth())
 
+def about():
+    return dict()
+
+def registration():
+    return dict()
+
+def activities():
+    return dict()
+
+def nurses():
+    return dict()
+
 def buySomethingElse():
     form = SQLFORM(db.pending, formstyle='divs', submit_button='Buy')
     if form.process().accepted:
@@ -49,16 +61,19 @@ def paypal():
     return dict()
 
 def success():
-    log_file(str(request.vars), 'tmp/paypal.return')
+    # log_file(str(request.vars), 'tmp/paypal.return')
     if request.args(0) == 'paypal':
-        if request.vars.payment.status == 'Complete':
-            message = 'Thank you! You payment is complete and your order is being prepared'
+        if response:
+            message = 'Thank you! You payment is complete and your order is being prepared.'
         else:
-            message = 'Sorry. An error has occured and you payment did not go through'
+            message = 'Sorry. An error has occured and you payment did not go through.'
     return dict(message=message)
 
-@auth.requires_login()
 def staff():
+    return dict()
+
+@auth.requires_login()
+def staff_app():
     form = SQLFORM(db.staff)
     if form.process().accepted:
         response.flash = 'form accepted'
@@ -68,8 +83,11 @@ def staff():
         response.flash = 'please fill out the form'
     return dict(form=form)
 
-@auth.requires_login()
 def counselor():
+    return dict()
+
+@auth.requires_login()
+def counselor_app():
     form = SQLFORM(db.counselor)
     if form.process().accepted:
         response.flash = 'form accepted'
@@ -79,11 +97,15 @@ def counselor():
         response.flash = 'please fill out the form'
     return dict(form=form)
 
-@auth.requires_login()
 def camper():
+    return dict()
+
+@auth.requires_login()
+def camper_app():
     form = SQLFORM(db.camper)
     if form.process().accepted:
         response.flash = 'form accepted'
+        redirect(URL('default', 'guardian'))
     elif form.errors:
         response.flash = 'form has errors'
     else:
@@ -95,6 +117,7 @@ def guardian():
     form = SQLFORM(db.guardian)
     if form.process().accepted:
         response.flash = 'form accepted'
+        redirect(URL('default', 'medical_information'))
     elif form.errors:
         response.flash = 'form has errors'
     else:
@@ -106,6 +129,7 @@ def medical_information():
     form = SQLFORM(db.medical_information)
     if form.process().accepted:
         response.flash = 'form accepted'
+        redirect(URL('default', 'eating'))
     elif form.errors:
         response.flash = 'form has errors'
     else:
@@ -117,6 +141,7 @@ def eating():
     form = SQLFORM(db.eating)
     if form.process().accepted:
         response.flash = 'form accepted'
+        redirect(URL('default', 'behavior'))
     elif form.errors:
         response.flash = 'form has errors'
     else:
@@ -128,6 +153,7 @@ def behavior():
     form = SQLFORM(db.behavior)
     if form.process().accepted:
         response.flash = 'form accepted'
+        redirect(URL('default', 'transportation'))
     elif form.errors:
         response.flash = 'form has errors'
     else:
@@ -139,6 +165,7 @@ def transportation():
     form = SQLFORM(db.transportation)
     if form.process().accepted:
         response.flash = 'form accepted'
+        redirect(URL('default', 'new_camper_information'))
     elif form.errors:
         response.flash = 'form has errors'
     else:
@@ -150,6 +177,7 @@ def new_camper_information():
     form = SQLFORM(db.new_camper_information)
     if form.process().accepted:
         response.flash = 'form accepted'
+        redirect(URL('default', 'camp_parent_questionnaire'))
     elif form.errors:
         response.flash = 'form has errors'
     else:
@@ -161,11 +189,21 @@ def camp_parent_questionnaire():
     form = SQLFORM(db.camp_parent_questionnaire)
     if form.process().accepted:
         response.flash = 'form accepted'
+        redirect(URL('default', 'buySomethingElse'))
     elif form.errors:
         response.flash = 'form has errors'
     else:
         response.flash = 'please fill out the form'
     return dict(form=form)
+
+def donate():
+    return dict()
+
+def wishlist():
+    return dict()
+
+def questions():
+    return dict()
 
 @cache.action()
 def download():
